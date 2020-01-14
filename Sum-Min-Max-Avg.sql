1cb2acac63ee63d68f6e963b81803ba2625092e6
Bruno Henrique de Oliveira <43800811+brunohein123@users.noreply.github.com>
2020-01-13 21:08:25 -0300

    Add files via upload

---
+++ Sum-Min-Max-Avg.sql
@@ -0,0 +1,11 @@
+SELECT TOP 10 sum(colunaParaOperacao) AS "Soma"
+FROM TabelaParaOperacao
+
+SELECT TOP 10 MIN(colunaParaOperacao) AS "Minimo"
+FROM TabelaParaOperacao
+
+SELECT TOP 10 MAX(colunaParaOperacao) AS "Maximo"
+FROM TabelaParaOperacao
+
+SELECT TOP 10 AVG(colunaParaOperacao) AS "Media"
+FROM TabelaParaOperacao
