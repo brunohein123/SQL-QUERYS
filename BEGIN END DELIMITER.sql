eee2052a01b59d7588ce7162961fcafd12ace99a
Bruno Henrique de Oliveira <43800811+brunohein123@users.noreply.github.com>
2020-01-20 22:46:47 -0300

    Add files via upload

---
+++ BEGIN END DELIMITER.sql
@@ -0,0 +1,23 @@
+DELIMITER $$
+CREATE FUNCTION soma (valorUm INT, valorDois INT)
+RETURNS INT
+BEGIN
+	RETURN valorUm + valorDois;
+END$$
+
+DELIMITER ;
+SELECT soma(1,2) AS "SOMA";
+
+DELIMITER $$
+
+CREATE PROCEDURE buscaAlgo (nome varchar(50))
+BEGIN
+	SELECT * 
+    FROM tabelaNome AS Resultado
+    WHERE nome LIKE('_runo') AND listaDeNomes LIKE(nome);
+    SELECT "Busca realizada com sucesso";
+END$$
+
+DELIMITER ;  
+
+CALL buscaAlgo
